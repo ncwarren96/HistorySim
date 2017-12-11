38bb246c012e247253a84a32c7300c991cd299fd
Nick Warren <nick.warren1996@gmail.com>
2017-12-10 23:30:55 -0800

    gave Tiles neighor arrays and tried to search them

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
-const GRID_SIZE = 10;
+const GRID_SIZE = 50;
 const NUMAGENTS = 1;
 
 let agents = [];
 let tiles = [];
 let graph;
+let path = [];
 
 let canvas;
 let slider, buttonStop, buttonClear
@@ -15,33 +16,46 @@ function setup() {
 	frameRate(60);
 
 	setupControls();
-	let graph = new Graph();
+	//let graph = new Graph();
 
 	colorMode(HSB);
 	background(0);
 	noStroke();
-	
-	let index = 0
+
+	//init array of Tiles with GRID_SIZE
+	let index = 0;
 	for(let i=0; i<width; i+=GRID_SIZE){
 		for(let j=0; j<height; j+=GRID_SIZE){
-			tiles.push(new Tile(i,j));
-			graph.addVertex(index);
-
+			tiles.push(new Tile(i, j, index));
 			index++;
+			//graph.addVertex(index);
 		}
 	}
-	for(let i=0; i<graph.vertices.length; i++){
-		if((i+1)%(width/GRID_SIZE) !=0){ 
-			graph.addEdge(i, i+1); //add edges horizontally
 
-		}
-		if(i-2900 < 0){
-			graph.addEdge(i, i+50); //add edges vertically
+	//init neighbor list for each Tile
+	for(let i=0; i<tiles.length; i++){
+		let t = tiles[i];
+
+		for(let j=0; j<tiles.length; j++){
+			let u = tiles[j];
+			if(t.neighbors.indexOf(u) < 0){
+				if(u.x == t.x+GRID_SIZE && u.y == t.y){
+					t.neighbors.push(u);
+				}
+				if(u.x == t.x-GRID_SIZE && u.y == t.y){
+					t.neighbors.push(u);
+				}
+				if(u.x == t.x && u.y == t.y+GRID_SIZE){
+					t.neighbors.push(u);
+				}
+				if(u.x == t.x && u.y == t.y-GRID_SIZE){
+					t.neighbors.push(u);
+				}
+			}
 		}
 	}
-	console.log(graph.pathFromTo(5, 500));
-	
-
+	console.log(tiles);
+	//init list of NUMAGENTS agents 
 	for(let i=0; i<NUMAGENTS; i++){
 		let r = random(0,4);
 		if(r<1){
@@ -54,6 +68,33 @@ function setup() {
 			agents.push(new Agent(random(0,height), height)); //bottom
 		}
 	}
+// 		if(tiles[i+1]){
+// 			if(t.x == tiles[i+1].x){ 
+// 				t.neighbors.push(tiles[i+1]); //right neighbor
+// 			}
+// 		}
+// 		if(tiles[i-1]){
+// 			if(t.x == tiles[i-1].x){
+// 				t.neighbors.push(tiles[i-1]);
+// 			}
+// 		}
+// 		if(tiles[]){
+			
+// 		}
+
+// 	for(let i=0; i<graph.vertices.length; i++){
+// 		if((i+1)%(width/GRID_SIZE) !=0){ 
+// 			graph.addEdge(i, i+1); //add edges horizontally
+// 		}
+// 		if(i-2900 < 0){
+// 			graph.addEdge(i, i+50); //add edges vertically
+// 		}
+// 	}
+// 	console.log(graph.pathFromTo(5, 500));
+
+	path = search(tiles, 1, 3);
+	console.log(path);
+
 }
 
 function setupControls(){
@@ -85,6 +126,42 @@ function clearTiles(){
 	}
 }
 
+function search(grid, start, dest){
+	let q = new PriorityQueue();
+	q.push(start, 0);
+
+	let previous = {};
+	let cost_so_far = {};
+	previous[start] = null;
+	cost_so_far[start] = 0;
+
+	while(q.size() > 0){
+		let current = q.pop();
+		console.log("at tile", current);
+
+		if(current == dest){
+			return previous;
+		}
+
+		for(let i=0; i<grid[current].neighbors.length; i++){
+			let next = grid[current].neighbors[i].i;
+			//let new_cost = cost_so_far[current] + grid[next].decay;
+
+			if(previous[next] == undefined){
+				q.push(next, 0);
+				previous[next] = current;
+			}
+			// if(cost_so_far[next] == undefined || new_cost < cost_so_far[next]){
+			// 	cost_so_far[next] = new_cost;
+			// 	let priority = new_cost
+			// 	q.push(next, priority);
+			// 	previous[next] = current;
+			// }
+		}
+	}
+	return previous;
+}
+
 function draw() {
 	//frameRate(slider.value());
 	for (let i=0; i<tiles.length; i++){
@@ -97,7 +174,7 @@ function draw() {
 				a.y < tiles[i].y+GRID_SIZE+2 &&
 				a.y+GRID_SIZE+2 > tiles[i].y){
 
-					tiles[i].decay -=1;
+					tiles[i].decay +=1;
 				}
 			}
 		}
@@ -126,8 +203,16 @@ function draw() {
 			}
 		}
 	}
+	stroke(255);
+	for(let key in path){
+		let a = tiles[key]
+		if(tiles[path[key]] != null){
+			let b = tiles[path[key]]
+			line(a.x, a.y, b.x, b.y);
+		}
+	}
+	noStroke();
 }
-
 
 //---OBJECTS---
 
@@ -156,14 +241,33 @@ function Agent(ix, iy){
 };
 
 //Tile class
-function Tile(ix, jy){
+function Tile(ix, jy, ii){
 	this.x = ix;
 	this.y = jy;
-	this.decay = 110;
-	this.touched = false;
+	this.i = ii;
+	this.decay = 0;
+	this.neighbors = [];
 
 	this.display = function(){
-		fill(this.decay, 75, 80);
+		fill(110-this.decay, 75, 80);
 		rect(this.x,this.y,GRID_SIZE, GRID_SIZE);
 	};
 };
+
+function PriorityQueue() {
+  this.data = []
+}
+
+PriorityQueue.prototype.push = function(element, priority) {
+  priority = +priority
+  for (var i = 0; i < this.data.length && this.data[i][1] > priority; i++);
+  this.data.splice(i, 0, [element, priority])
+}
+
+PriorityQueue.prototype.pop = function() {
+  return this.data.shift()[0]
+}
+
+PriorityQueue.prototype.size = function() {
+  return this.data.length
+}
